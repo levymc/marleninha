0c33c85d9c54c704953c5614ef4796c3e50c36ec
Levy <levymcruz@gmail.com>
2023-09-28 20:41:39 -0300

    first commit

---
+++ src/data/fruits.ts
@@ -0,0 +1,16 @@
+import { Fruit } from "../repositories/fruits-repository";
+
+const fruits: Fruit[] = [
+    {
+        id: 3,
+        name: "maça",
+        price: 4,   
+    },
+    {
+        id: 3,
+        name: "maça",
+        price: 34
+    }
+];
+
+export default fruits;
